3c3e98d114828fb13eff28927723126684c9df3d
c9s <cornelius.howl@gmail.com>
2013-03-03 15:06:35 +0800

    First commit

---
+++ src/File/PrettySize.php
@@ -0,0 +1,20 @@
+<?php
+namespace File;
+
+class PrettySize
+{
+    static function fromBytes($bytes)
+    {
+        if( $bytes < 1024 )
+            return $bytes . ' B';
+        if( $bytes < 1024 * 1024 )
+            return ((int) $bytes / 1024) . ' KB';
+        if( $bytes < 1024 * 1024 * 1024 )
+            return ((int) $bytes / 1024 / 1024) . ' MB';
+        if( $bytes < 1024 * 1024 * 1024 * 1024 )
+            return ((int) $bytes / 1024 / 1024 / 1024) . ' GB';
+        return ((int) $bytes / 1024 / 1024) . ' MB';
+    }
+
+}
+
